f425838bc40fddc29b5fe784fc6eeef585096892
ankitdabhi <ankit.dabhi.ext@seamless.se>
2022-11-09 13:00:30 +0530

    PAN Matcher Added

---
+++ src/main/java/com/example/demo/core/java/CustomConstant.java
@@ -0,0 +1,9 @@
+package com.example.demo.core.java;
+
+public class CustomConstant {
+
+    public static final String PAN="CLRPD3753R";
+    public static final String PAN_PATTERN = "[A-Z]{3}[ABCFGHLJPTF]{1}[A-Z]{1}[0-9]{4}[A-Z]{1}";
+    public static final String MSG_PAN_MATCHED = "PAN Matched";
+    public static final String MSG_PAN_NOT_MATCHED = "PAN Not matched";
+}
